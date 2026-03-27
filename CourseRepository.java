f385849070d91285d67791891e39dbc2fe372c7a
Kevin Paulson <kevinpaulson983@gmail.com>
2026-03-27 19:30:17 +0530

    Delete CourseRepository.java

--- CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tracker.studentracker.repository;
-
-
-import com.tracker.studentracker.models.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
